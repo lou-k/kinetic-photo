b85ad7cf762dfe2cd687daa1311abcab72de59e8
Lou <lou@lou.dev>
2023-05-31 21:10:12 -0400

    Updated some more documentation stuff

--- server/src/kinetic_server/integrations/__init__.py
+++ server/src/kinetic_server/integrations/__init__.py
@@ -1,34 +1,75 @@
 import json
 from enum import Enum
 
-from ..db import IntegrationsDb
+import pandas as pd
 
+from ..db import IntegrationsDb
 from .common import Integration
 from .googlephotos import GooglePhotos
 
 
 class IntegrationType(Enum):
+    """Each supported integration should have a constant here indicating their type."""
+
     GOOGLEPHOTOS = GooglePhotos
 
 
 class IntegrationsApi:
+    """An API for managing integerations."""
+
     def __init__(self, db: IntegrationsDb):
         self._db = db
 
-    def list(self):
+    def list(self) -> pd.DataFrame:
+        """Lists all integrations in the database
+
+        Returns:
+            pd.DataFrame: A table of all integrations in the database.
+        """
         return self._db.list()
 
-    def get(self, id: int):
+    def get(self, id: int) -> Integration:
+        """Retrieves an integeation from the database
+
+        Args:
+            id (int): The integration to get
+
+        Returns:
+            Integration: The integration oject instance
+        """
         _, _, typ, params = self._db.get(id)
         return self.from_params(IntegrationType[typ], json.loads(params))
 
-    def from_params(self, type: IntegrationType, params):
+    def from_params(self, type: IntegrationType, params) -> Integration:
+        """Re-serializes an integration of the provided type using the provided params.
+
+        Args:
+            type (IntegrationType): The type of integration object to create
+            params (dict): The parameters to pass to the types constructor.
+
+        Returns:
+            Integration: The integration oject instance
+        """
         return type.value(**params)
 
     def remove(self, id: int):
+        """Deletes the indicated integration from the database
+
+        Args:
+            id (int): The id of the integration to delete.
+        """
         return self._db.remove(id)
 
-    def add(self, name: str, integration: Integration):
+    def add(self, name: str, integration: Integration) -> int:
+        """Saves a new integration in the database
+
+        Args:
+            name (str): The name of this integrtaion
+            integration (Integration): An instance of the integration to persist.
+
+        Returns:
+            int: The id of the newly created intgeration
+        """
         typ = next(t for t in IntegrationType if t.value == type(integration))
         params = json.dumps(integration.params())
         return self._db.add(name, typ.name, params)
--- server/src/kinetic_server/pipelines.py
+++ server/src/kinetic_server/pipelines.py
@@ -4,8 +4,8 @@ from tempfile import NamedTemporaryFile
 from typing import Iterator, List, Tuple
 
 import pandas as pd
-from disk_objectstore import Container
 import tqdm
+from disk_objectstore import Container
 
 from .common import PipelineRun, PipelineStatus, StreamMedia
 from .content import ContentApi
@@ -15,6 +15,17 @@ from .rules import Rule
 
 
 class PipelineLogger:
+    """A PipelineLogger records all logging events made during a Pipeline's run to a file and,
+    once the run is completed, saves that log along with the resulting status to the database.
+    Thus it persists errors and processing information for inspection later.
+
+    The logger is a context provider that returns a logging.Logger object. To use it:
+
+    with pipeline_logger as logger:
+        logger.info....
+    
+    Once the `with` clause completes the log is persisted to the data and object store.
+    """
     def __init__(
         self, db: PipelineDb, objectstore: Container, pipeline_id: int, name: str
     ):
@@ -76,6 +87,9 @@ class PipelineLogger:
 
 
 class PipelineLoggerFactory:
+    """A factory for PipelineLoggers.
+    This factory helps pipelines avoid passing around the PipelineDb and Container references.
+    """
     def __init__(self, db: PipelineDb, object_store: Container):
         self._db = db
         self._object_store = object_store
@@ -85,6 +99,16 @@ class PipelineLoggerFactory:
 
 
 class Pipeline:
+    """Pipelines process media from input streams and creates kinetic photos. It does this via it's steps:
+    a collection of (Rule, Processor) pairs that can create video clips for different kinds of content.
+
+    Each piece of media is evaluated against each Rule and, if it complies, is passed to the corresponding Processor.
+    If the Processor is successful and creates a video file, it is added to the Content database via the ContentApi 
+    and will be available to all Kinetic Frames.
+
+    Each time a pipeline is invoked via __call__, it's log and resulting status will be saved to the "pipeline_runs" table in the database.
+    See the PipelineApi for how to access these results.
+    """
     def __init__(
         self,
         id: int,
@@ -93,6 +117,15 @@ class Pipeline:
         content_api: ContentApi,
         logger_factory: PipelineLoggerFactory,
     ):
+        """Creates an instance of a Pipeline object
+
+        Args:
+            id (int): The pipeline's id
+            name (str): The Pipeline's name
+            steps (List[Tuple[Rule, Processor]]): Steps in this pipeline
+            content_api (ContentApi): The content api for saving content
+            logger_factory (PipelineLoggerFactory): A logger factory used to create Pipeline loggers.
+        """
         self.id = id
         self.name = name
         self.steps = steps
@@ -100,6 +133,15 @@ class Pipeline:
         self._content_api = content_api
 
     def __call__(self, stream: Iterator[StreamMedia], limit: int = None) -> None:
+        """Runs this Pipeline to convert stream media into kinetic photo content.
+
+        Args:
+            stream (Iterator[StreamMedia]): An iterator of media items to process.
+            limit (int, optional): If set, only process this many items. 
+
+        Raises:
+            Exception: If the pipeline fails all media an exception is thrown.
+        """
         pipeline_logger = self._logger_factory(self)
         with pipeline_logger as logger:
             num_successful = 0
@@ -149,30 +191,75 @@ class Pipeline:
 
 
 class PipelineApi:
+    """Provides programatic tools for managing pipelines.
+    """
     def __init__(
         self,
         db: PipelineDb,
         content_api: ContentApi,
         logger_factory: PipelineLoggerFactory,
     ):
+        """Creates a new instance of the PipelineApi
+
+        Args:
+            db (PipelineDb): The pipeline database
+            content_api (ContentApi): The content api used to create content
+            logger_factory (PipelineLoggerFactory): A factory for creating pipeline loggers.
+        """
         self._db = db
         self._content_api = content_api
         self._logger_factory = logger_factory
 
     def get(self, id: int) -> Pipeline:
+        """Retrieves a pipeline by it's id
+
+        Args:
+            id (int): The pipeline's id
+
+        Returns:
+            Pipeline: An instantiated pipeline object that represents this pipeline from the database.
+        """
         id, name, steps = self._db.get(id)
         return Pipeline(id, name, steps, self._content_api, self._logger_factory)
 
     def list(self) -> pd.DataFrame:
+        """Lists all pipelines in the database.
+
+        Returns:
+            pd.DataFrame: A dataframe containing all pipeline information.
+        """
         return self._db.list()
 
     def list_runs(self) -> pd.DataFrame:
+        """Lists all of the pipeline runs in the database
+
+        Returns:
+            pd.DataFrame: A table of all pipeline runs in the database.
+        """
         return self._db.list_runs()
 
     def create(self, name: str) -> Pipeline:
+        """Creates a new pipeline with the provided name.
+
+        Args:
+            name (str): The human-language name for this pipeline
+
+        Returns:
+            Pipeline: An instantiated pipeline class representing the new pipeline you created.
+        """
         id = self._db.create(name)
         return self.get(id)
 
     def add_step(self, pipeline_id: int, rule: Rule, processor: Processor) -> Pipeline:
+        """Adds a step to the indicated pipeline
+
+        Args:
+            pipeline_id (int): The pipeline to add the step to.
+            rule (Rule): The step's Rule.
+            processor (Processor): The step's Processor.
+
+        Returns:
+            Pipeline: A new pipeline object with the provided step included.
+        """
         self._db.add_step(pipeline_id=pipeline_id, rule=rule, processor=processor)
         return self.get(pipeline_id)
