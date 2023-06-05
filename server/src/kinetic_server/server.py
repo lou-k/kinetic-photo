015f7eb602d058e0ccd6f7ec9dc9714a16ddca70
Lou <lou@lou.dev>
2023-06-05 19:03:09 -0400

    Add playlist option in server, update client config settings

--- server/src/kinetic_server/server.py
+++ server/src/kinetic_server/server.py
@@ -2,22 +2,21 @@ import argparse
 import json
 import logging
 import traceback
+from dataclasses import dataclass
 from http.client import HTTPException
 from typing import List
 
+from dataclasses_json import dataclass_json
 from dependency_injector.wiring import Provide, inject
-from flask import Flask, Response, jsonify, request, render_template
-from kinetic_server.common import Content, Frame
 from disk_objectstore import Container as DiskContainer
+from flask import Flask, Response, jsonify, render_template, request
 
-
+from kinetic_server.common import Content, Frame
 from kinetic_server.content import ContentApi
 from kinetic_server.frames import FramesApi
 
 from .containers import Container
 
-from dataclasses_json import dataclass_json
-from dataclasses import dataclass
 
 #
 # API types
@@ -28,9 +27,11 @@ class GetFrameResult:
     frame: Frame
     content: List[Content]
 
+
 #
 # Endpoints
 #
+
 
 @inject
 def frame(
@@ -42,18 +43,27 @@ def frame(
     resp = GetFrameResult(frame=frame, content=content)
     return resp.to_dict()
 
+
 @inject
-def video(
-    id: str,
-    object_store: DiskContainer = Provide[Container.object_store]
-):
+def playlist(id: str, frames_api: FramesApi = Provide[Container.frames_api]):
+    logging.info(f"Id is {id}")
+    content = frames_api.get_content_for(id)
+    res = "#EXTM3U\n"
+    for c in content:
+        res += f"#EXINF:\n{request.url_root}video/{c.id}\n"
+    res += "#EXT-X-ENDLIST"
+    return res, 200, {"Content-Type": "video/mp4"}
+
+
+@inject
+def video(id: str, object_store: DiskContainer = Provide[Container.object_store]):
     if object_store.has_object(id):
         video_object = object_store.get_object_content(id)
         # TODO -- ensure that the content type is correct - maybe store it in the objectstore?
-        return video_object, 200, {'Content-Type': 'video/mp4'}
+        return video_object, 200, {"Content-Type": "video/mp4"}
     else:
         return {}, 404
-     
+
 
 def create_server(environ=None, start_response=None):
     container = Container()
@@ -63,6 +73,7 @@ def create_server(environ=None, start_response=None):
     app.container.wire(modules=[__name__])
     app.add_url_rule("/frame/<id>", "frame", frame)
     app.add_url_rule("/video/<id>", "video", video)
+    app.add_url_rule("/playlist/<id>.m3u8", "playlist", playlist)
 
     return app
 
