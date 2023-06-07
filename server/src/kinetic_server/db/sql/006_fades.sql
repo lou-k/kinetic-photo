36ae1b0302fea0209a06724f35b47c7628d7ca91
Lou <lou@lou.dev>
2023-06-07 13:33:46 -0400

    Forgot src

---
+++ server/src/kinetic_server/db/sql/006_fades.sql
@@ -0,0 +1,2 @@
+ALTER TABLE content
+ADD COLUMN faded_hash text;
---
+++ server/src/kinetic_server/fader.py
@@ -0,0 +1,51 @@
+import logging
+import subprocess
+from tempfile import NamedTemporaryFile
+from typing import Tuple
+
+
+def get_video_duration(filename: str) -> float:
+    result = subprocess.run(
+        [
+            "ffprobe",
+            "-v",
+            "error",
+            "-show_entries",
+            "format=duration",
+            "-of",
+            "default=noprint_wrappers=1:nokey=1",
+            filename,
+        ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+    )
+    return float(result.stdout)
+
+
+def fade_video(video_bytes: bytes, fade_duration: float = 1, video_bitrate: int = 1200) -> Tuple[bytes, float]:
+    with NamedTemporaryFile() as tmpfile:
+        with open(tmpfile.name, "wb") as fout:
+            fout.write(video_bytes)
+        video_duration = get_video_duration(tmpfile.name)
+        with NamedTemporaryFile(suffix=".mp4") as resultfile:
+            cmd = [
+                "ffmpeg",
+                "-i",
+                tmpfile.name,
+                "-vf",
+                f"fade=t=in:st=0:d={fade_duration},fade=t=out:st={video_duration - fade_duration}:d={fade_duration}",
+                "-b:v",
+                f"{video_bitrate}k",
+                "-c:a",
+                "copy",
+                "-f",
+                "mp4",
+                "-movflags",
+                "+faststart",
+                "-y",
+                resultfile.name,
+            ]
+            logging.info(f"Fading video with command:" + " ".join(cmd))
+            subprocess.run(cmd, check=True)
+            with open(resultfile.name, "rb") as fin:
+                return (fin.read(), video_duration)
